9466b75415ecea3eb4e1ff48390496107bfd4dc7
myewebgit <mnislamctg7@gmail.com>
2022-02-17 18:41:12 +0600

    forth commit

--- script.js
+++ script.js
@@ -27,3 +27,53 @@ const newrBalanceTotal = newBalanceTotal - savings;
 rbalanceTotal.innerText = newrBalanceTotal;
 
   })
+
+  // ***********calculation first firt**********
+
+  document.getElementById('calculate').addEventListener('click',function(){
+    const depositInput = document.getElementById('income-input');
+     const depositAmountText = depositInput.value;
+const depositAmount = parseFloat(depositAmountText)
+
+// depositInput.value = '';
+
+const foodInput = document.getElementById('food-input');
+const foodAmountText = foodInput.value;
+const foodAmount = parseFloat(foodAmountText);
+
+foodInput.value = '';
+
+
+const rentInput = document.getElementById('rent-input');
+const rentAmountText = rentInput.value;
+const rentAmount = parseFloat(rentAmountText);
+
+rentInput.value = '';
+
+
+const clothInput = document.getElementById('cloth-input');
+const clothAmountText = clothInput.value;
+const clothAmount = parseFloat(clothAmountText);
+
+clothInput.value = '';
+
+
+const expencesTotal = document.getElementById('expences-total');
+const expencesTotalText =expencesTotal.innerText;
+const previousexpencesTotal = parseFloat(expencesTotalText);
+const newexpencesTotal =(rentAmount + foodAmount + clothAmount);
+expencesTotal.innerText = newexpencesTotal;
+
+
+const balanceTotal = document.getElementById('balance-total');
+const balanceTotalText =balanceTotal.innerText;
+const previousBalanceTotal = parseFloat(balanceTotalText);
+const newBalanceTotal = depositAmount - newexpencesTotal;
+balanceTotal.innerText = newBalanceTotal;
+
+
+
+
+
+
+});
